b5a8b5e95f78de10786210247143dc217b392275
Gabriel Pontes <gabrielpontescontato1@gmail.com>
2022-08-17 03:00:22 -0300

    Backgrounds,Emojis e SoundEffect

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ console.log("Script inicializado!");
 
 const MeninasTitulo = document.querySelector(".Meninas");
 const ViloesTitulo = document.querySelector(".Viloes");
-
+const Florzinha = document.querySelector(".Florzinha");
+const Docinho = document.querySelector(".Docinho");
+const Lindinha = document.querySelector(".Lindinha");
+const Macaco = document.querySelector(".Macaco");
+const Ele = document.querySelector(".Ele");
+const MaisGrana = document.querySelector(".MaisGrana");
+const Sedusa = document.querySelector(".Sedusa");
+const Desordeiros = document.querySelector(".Desordeiros");
+const soundEffect = new Audio('/sound/starsound.mp3');
 
 setInterval(() => {
     const ColorsMeninasTitulo = ['green','red','blue'];
@@ -28,6 +36,59 @@ function EvilTitle(whatever){
         ViloesTitulo.style.color = "black";
         ViloesTitulo.style.backgroundColor = "#fff";
 
+    }
+}
+
+
+function Acao(obj){
+    soundEffect.play();
+
+    if(obj == "Florzinha"){
+        Florzinha.innerHTML = "&#127802; Florzinha &#127802;";
+        Florzinha.style.fontWeight = "bold";
+        Florzinha.style.backgroundImage = "url('/img/florzinha-bg.gif')";
+    }
+
+    if(obj == "Docinho"){
+        Docinho.innerHTML = "&#127852; Docinho &#127852;";
+        Docinho.style.fontWeight = "bold";
+        Docinho.style.backgroundImage = "url('/img/docinho-bg.webp')";
+    }
+
+    if(obj == "Lindinha"){
+        Lindinha.innerHTML = "&#127776; Lindinha &#127776;";
+        Lindinha.style.fontWeight = "bold";
+        Lindinha.style.backgroundImage = "url('/img/lindinha-bg.webp')";
+    }
+
+    if(obj == "Macaco"){
+        Macaco.innerHTML = "&#129421; Macaco Louco &#129421;";
+        Macaco.style.fontWeight = "bold";
+        Macaco.style.backgroundImage = "url('/img/macaco-bg.gif')";
+    }
+
+    if(obj == "Ele"){
+        Ele.innerHTML = "&#129408; Ele &#129408;";
+        Ele.style.fontWeight = "bold";
+        Ele.style.backgroundImage = "url('/img/ele-bg.gif')";
+    }
+
+    if(obj == "MaisGrana"){
+        MaisGrana.innerHTML = "&#128181; Princesa MaisGrana &#128181;";
+        MaisGrana.style.fontWeight = "bold";
+        MaisGrana.style.backgroundImage = "url('/img/maisgrana-bg.gif')";
+    }
+
+    if(obj == "Sedusa"){
+        Sedusa.innerHTML = "&#128131;&#127995; Sedusa &#128131;&#127995;";
+        Sedusa.style.fontWeight = "bold";
+        Sedusa.style.backgroundImage = "url('/img/sedusa-bg.gif')";
+    }
+
+    if(obj == "Desordeiros"){
+        Desordeiros.innerHTML = "&#129490;&#127995;&#129490;&#127997;&#129490;&#127996; Meninos Desordeiros &#129490;&#127995;&#129490;&#127997;&#129490;&#127996;"
+        Desordeiros.style.fontWeight = "bold";
+        Desordeiros.style.backgroundImage = "url('/img/desordeiros-bg.jpg')";
     }
 }
 
